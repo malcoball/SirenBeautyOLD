6e421f0d185f9c96db042322c867565aa01a5b49
malcoball <adam_shaw1994@hotmail.co.uk>
2022-08-10 09:27:08 +0100

    Start on mobile design next

--- domGen.js
+++ domGen.js
@@ -86,7 +86,9 @@ function Portfolio(){
     const header = makeElm('h1',"","",'Gallery');
     const para = makeElm('p',"","",'All pigments and tools used are safe, sterile, high end and carefully chosen for the treatments provided.');
     const button = makeElm('button',"","","See Siren Beauty's portfolio");
-    // button.onClick = 
+    button.addEventListener("click",()=>{
+      window.open("https://www.instagram.com/sirenbeautynottingham/",'_blank');
+    });
   const background = makeElm('background','col1');
     const backgroundInner = makeElm('div',"background")
     backgroundInner.style.backgroundImage = 'url("Images/portBackground.jpg")';
--- serviceCard.js
+++ serviceCard.js
@@ -1,15 +1,17 @@
 function ServiceCard(titleText,subHeadText,paraText,imageSrc){
     const mainCon = makeElm('div','cardContainer col2');
       const leftCon = makeElm('div','leftDiv cardDiv');
-        const title = makeElm('h3',"",'title',titleText);
-        const subHead = makeElm('h5',"",'subHead',subHeadText);
-        const para    = makeElm('p',"","",paraText);
+        const leftConSpace = makeElm('div','leftDivInner');
+          const title = makeElm('h3',"",'title',titleText);
+          const subHead = makeElm('h5',"",'subHead',subHeadText);
+          const para    = makeElm('p',"","",paraText);
       const rightCon = makeElm('div','rightDiv cardDiv');
         if (imageSrc === null) imageSrc = 'url("Images/cardImage.jpg")';
         rightCon.style.backgroundImage = imageSrc;
 
     mainCon.append(leftCon,rightCon);
-      leftCon.append(title,subHead,para);
+      leftCon.append(leftConSpace);
+        leftConSpace.append(title,subHead,para);
     
     return mainCon;
 }
