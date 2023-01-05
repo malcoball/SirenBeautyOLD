a557d95f7aba23768f122924b2c01839778416a9
AdamShaw <59421635+malcoball@users.noreply.github.com>
2023-01-05 20:11:29 +0000

    Changed the cards so they're in a grid, still needs work to be dynamic though.

--- Components/Elements/ServiceCard.js
+++ Components/Elements/ServiceCard.js
@@ -2,18 +2,22 @@ import makeElm from "../MakeElm.js";
 
 export default function ServiceCard(titleText,subHeadText,paraText,imageSrc){
     const mainCon = makeElm('div','cardContainer col2');
-      const leftCon = makeElm('div','leftDiv cardDiv');
+      const leftCon = makeElm('div','leftDiv innerCardCont');
         const leftConSpace = makeElm('div','leftDivInner');
           const title = makeElm('h3',"",'title',titleText);
           const subHead = makeElm('h5',"",'subHead',subHeadText);
           const para    = makeElm('p',"","",paraText);
-      const rightCon = makeElm('div','rightDiv cardDiv');
-        if (imageSrc === null) imageSrc = 'url("Images/cardImage.jpg")';
-        rightCon.style.backgroundImage = imageSrc;
+      const midCon = makeElm('div','midDiv');
+      const rightCon = makeElm('div','rightDiv innerCardCont');
+        const backImage = makeElm('div','rightDivImage');
 
-    mainCon.append(leftCon,rightCon);
+        if (imageSrc === null) imageSrc = 'url("Images/cardImage.jpg")';
+        backImage.style.backgroundImage = imageSrc;
+
+    mainCon.append(leftCon,midCon,rightCon);
       leftCon.append(leftConSpace);
         leftConSpace.append(title,subHead,para);
+      rightCon.append(backImage);
     
     return mainCon;
 }
--- Components/Pages/Services/Article.js
+++ Components/Pages/Services/Article.js
@@ -2,13 +2,17 @@ import makeElm from "../../MakeElm.js";
 import ServiceCard from "../../Elements/ServiceCard.js";
 
 const Article = ()=>{
-    const article = makeElm('article','col1');
-        article.append(ServiceCard("Powder/Ombre Machine Brows","£150 inc top up","Pigment is implanted into the brows using a dusting technique with a machine. This created a soft airy powdery look that can be enjoyed for 18/24 months",'url("Images/cardImageOmbre.jpg")'));
-        article.append(ServiceCard("MICROBLADED BROWS","£150 inc top up","The au natual look” Strokes are strategically placed using a manual tool to mimick the brows natural hairs. Results can be expected to last 12 months",'url("Images/cardImageMicro.jpg")'));
-        article.append(ServiceCard("COMBINATION BROWS","£150 inc top up","Skin is packed with pigment to enhance Colour for the lips. Perfectly compliments fillers for extra wow factor.",'url("Images/cardImageCombo.jpg")'));
-        article.append(ServiceCard("LIP BLUSH COMING SOON!","£150 inc top up","Pigment is implanted into the brows using a dusting technique with a machine. This created a soft airy powdery look that can be enjoyed for 18/24 months",'url("Images/cardImageLip.jpg")'));
-        article.append(ServiceCard("MICRONEEDLING","£40 or 3 sessions £100","Also known as collagens induction therepy. This treatment results in new fresh glowing skin. Creating tiny controlled injuries in the skin allows the healing process to take form. As this happens the skin is rejuvenated with new skin cells and makes the face appear more youthful. A series of treaments are reccomended for best results.",'url("Images/cardImageMicroN.jpg")'));
-        article.append(ServiceCard("REMOVALS","From £40 per session,<br> Emergency removal £150","Removals: To remove old or unwanted permanent make up or small body tattoos. A specialised solution is tattooed into the skin to cause an osmotic reaction to lift and fade unwanted pigment. Emergency removals have to to done within 48 hours of treatment.",'url("Images/cardImageRemoval.jpg")'));
+
+    const article = makeElm('article','col1 ');
+        const container = makeElm('div','cardDiv');
+            article.append(container);
+        container.append(ServiceCard("Powder/Ombre Brows","£150 inc top up","Pigment is implanted into the brows using a dusting technique with a machine. This created a soft airy powdery look that can be enjoyed for 18/24 months",'url("Images/cardImageOmbre.jpg")'));
+        container.append(ServiceCard("MICROBLADED BROWS","£150 inc top up","The au natual look” Strokes are strategically placed using a manual tool to mimick the brows natural hairs. Results can be expected to last 12 months",'url("Images/cardImageMicro.jpg")'));
+        container.append(ServiceCard("COMBINATION BROWS","£150 inc top up","Skin is packed with pigment to enhance Colour for the lips. Perfectly compliments fillers for extra wow factor.",'url("Images/cardImageCombo.jpg")'));
+        container.append(ServiceCard("LIP BLUSH COMING SOON!","£150 inc top up","Pigment is implanted into the brows using a dusting technique with a machine. This created a soft airy powdery look that can be enjoyed for 18/24 months",'url("Images/cardImageLip.jpg")'));
+        container.append(ServiceCard("MICRONEEDLING / collagens induction therepy","£40 or 3 sessions £100","This treatment results in new fresh glowing skin. Creating tiny controlled injuries in the skin allows the healing process to take form. As this happens the skin is rejuvenated with new skin cells and makes the face appear more youthful. A series of treaments are reccomended for best results.",'url("Images/cardImageMicroN.jpg")'));
+        container.append(ServiceCard("REMOVALS","From £40 per session, Emergency removal £150","Removals: To remove old or unwanted permanent make up or small body tattoos. A specialised solution is tattooed into the skin to cause an osmotic reaction to lift and fade unwanted pigment. Emergency removals have to to done within 48 hours of treatment.",'url("Images/cardImageRemoval.jpg")'));
+
     return article;
 }
 export default Article;
